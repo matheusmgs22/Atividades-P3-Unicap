8310362e595e06b69f49ee22fda2cb52e07e0f95
matheus <matheusmgs17@gmail.com>
2023-03-13 17:42:44 -0300

    atividade distancia

---
+++ Atividade-Distancia/src/Triangulo.java
@@ -0,0 +1,40 @@
+public class Triangulo {
+    Ponto p1;
+    Ponto p2;
+    Ponto p3;
+
+   public Triangulo(Ponto p1, Ponto p2, Ponto p3){
+    this.p1 = p1;
+    this.p2 = p2;
+    this.p3 = p3;
+
+
+   }
+
+   public double area(){
+    double determinantes, resposta;
+    double positivos , negativos;
+
+    positivos = (p1.x + p2.y) + (p1.y + p3.x) + (p2.x + p3.y);
+    negativos = (p2.y + p3.x) + (p1.x + p3.y) + (p1.y + p2.x);
+
+    determinantes = positivos - negativos;
+
+    determinantes = Math.abs(determinantes);
+
+    resposta  = determinantes / 2;
+
+    return resposta;
+
+   }
+
+   public double perimetro(){
+    double resposta;
+
+    resposta = p1.distancia(p2) + p2.distancia(p3) + p3.distancia(p1);
+
+    return resposta;
+
+   }
+   
+}
